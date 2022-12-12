be1e85b6cae2a475463dc934825b458e936e110c
BaoHoang0311 <tan.thach36@gmail.com>
2022-12-12 18:24:58 +0700

    learn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DetailsMovieComponent } from './components/details-movie/details-movie.component';
+import { DetailsReviewComponent } from './components/details-review/details-review.component';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
+import { SearchviewComponent } from './components/searchview/searchview.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ModelPopUpComponent } from './components/model-pop-up/model-pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailsComponent,
+    DetailsMovieComponent,
+    DetailsReviewComponent,
+    NotAuthorizedComponent,
+    SearchviewComponent,
+    LoaderComponent,
+    ModelPopUpComponent,
   ],
   imports: [
     BrowserModule,
